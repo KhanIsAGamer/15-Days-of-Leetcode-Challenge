5a2d55e8f17553f5bc7e8531cae1d229a820f683
Mahmood Ashraf <77431514+KhanIsAGamer@users.noreply.github.com>
2024-12-04 20:17:33 +0500

    Time: 1 ms (100.00%) | Memory: 42.1 MB (14.11%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,24 @@
+class Solution {
+   public static char findTheDifference(String s, String t) {
+        char result = 0;
+
+        for (char c : s.toCharArray()) {
+            result ^= c;
+        }
+        for (char c : t.toCharArray()) {
+            result ^= c;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abcd";
+        String t1 = "abcde";
+        System.out.println(findTheDifference(s1, t1)); 
+
+        String s2 = "";
+        String t2 = "y";
+        System.out.println(findTheDifference(s2, t2)); 
+    }
+}
